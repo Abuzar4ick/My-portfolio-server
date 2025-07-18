69616e3ab4c39a98ed0b5fb5c7d5c1e91558c4f8
Abuzar <abuzarrrakhmonov@gmail.com>
2025-07-18 23:55:04 +0500

    Some error fixed

--- src/services/dashboard.service.js
+++ src/services/dashboard.service.js
@@ -3,23 +3,31 @@ const { ProfileInfo, Social, Project, Experience, Education, Skill, SoftSkill } 
 // GET: /all-info/length
 // For: admin
 exports.getInformationLength = async () => {
-    const [profileCount, socialCount, projectCount, experienceCount, educationCount, skillCount, softSkillCount] = await Promise.all([
-      ProfileInfo.count(),
-      Social.count(),
-      Project.count(),
-      Experience.count(),
-      Education.count(),
-      Skill.count(),
-      SoftSkill.count()
-    ])
+  const [
+    profileCount,
+    socialCount,
+    projectCount,
+    experienceCount,
+    educationCount,
+    skillCount,
+    softSkillCount
+  ] = await Promise.all([
+    ProfileInfo.countDocuments(),
+    Social.countDocuments(),
+    Project.countDocuments(),
+    Experience.countDocuments(),
+    Education.countDocuments(),
+    Skill.countDocuments(),
+    SoftSkill.countDocuments()
+  ])
 
-    res.json({
-      profileInfo: profileCount,
-      social: socialCount,
-      project: projectCount,
-      experience: experienceCount,
-      education: educationCount,
-      skill: skillCount,
-      softSkill: softSkillCount
-    });
+  return {
+    profileInfo: profileCount,
+    social: socialCount,
+    project: projectCount,
+    experience: experienceCount,
+    education: educationCount,
+    skill: skillCount,
+    softSkill: softSkillCount
+  }
 }
