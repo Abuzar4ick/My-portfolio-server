2dd22decff58fc0afeaffeabb3e5c043981238aa
Abuzar <abuzarrrakhmonov@gmail.com>
2025-07-19 17:54:45 +0500

    Cors changed for test

--- src/app.js
+++ src/app.js
@@ -16,11 +16,13 @@ const {
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-// app.use(cors({
+app.use(cors(
+  // {
 //   origin: 'http://localhost:5173',
 //   credentials: true,
 //   allowedHeaders: ['Content-Type', 'Authorization']
-// }))
+  // }
+))
 
 app.use('/api',
   profileInfoRoute,
